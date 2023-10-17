960596736324d16e07debaf28d68b6b6d83b6d40
Awalhossain <waqin21@gmail.com>
2023-10-17 12:41:21 +0600

    refactor: code refactor ☑️

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import { userRoutes } from '../modules/user/user.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "/",
-    routes: "am"
+    path: "/user",
+    routes: userRoutes,
   }
 ];
 
-moduleRoutes.forEach(route => router.use(route.path,));
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;
