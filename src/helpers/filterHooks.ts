938ca8cf187cc621f4473924a326c1a949932d78
Awalhossain <waqin21@gmail.com>
2023-10-17 12:48:07 +0600

    feat: created custom hook for filtering & searching 🔍

---
+++ src/helpers/filterHooks.ts
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
+
+const searchFilter = (
+    searchTerm: string | undefined,
+    searchableFields: string[],
+) => {
+    const andCondition = ({
+        OR: searchableFields.map((field) => ({
+            [field]: {
+                contains: searchTerm,
+                mode: 'insensitive',
+            },
+        })),
+    });
+
+    return andCondition
+}
+
+
+
+type FilterOption = Record<string, any>;
+
+type RelationalFieldMapper = Record<string, string>;
+
+type Filter = {
+    [key: string]: {
+        equals: any;
+    } | {
+        [key: string]: {
+            id: string;
+        };
+    };
+};
+
+const objectFilter = (filterOption: FilterOption, relationalFields: string[], relationalFieldMapper: RelationalFieldMapper): Filter[] => {
+    const result = Object.keys(filterOption).map((key) => {
+        if (relationalFields.includes(key)) {
+            return {
+                [relationalFieldMapper[key]]: {
+                    id: filterOption[key]
+                }
+            };
+        } else {
+            return {
+                [key]: {
+                    equals: filterOption[key]
+                }
+            };
+        }
+    });
+
+    return result;
+};
+
+const filterCondition = (filterData: any) => {
+    const result = Object.keys(filterData).map((key) => (
+        {
+            [key]: {
+                equals: (filterData as any)[key]
+            }
+
+        }
+
+    ))
+    return result;
+}
+
+
+
+export const FilterOption = {
+    searchFilter,
+    objectFilter,
+    filterCondition
+} 
